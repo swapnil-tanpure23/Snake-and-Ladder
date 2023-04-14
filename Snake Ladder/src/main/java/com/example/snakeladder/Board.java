d9f57aaea2286dc921650fcc0358fff916d420e7
swapnil-tanpure23 <swapniltanpure74@gmail.com>
2023-04-14 08:44:10 +0530

    created Board logic and locate X and Y cordinate on board

---
+++ Snake Ladder/src/main/java/com/example/snakeladder/Board.java
@@ -0,0 +1,43 @@
+package com.example.snakeladder;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class Board {
+    ArrayList<Pair<Integer,Integer>> PositionCordinates;
+
+    public Board(){
+        PositionCordinates = new ArrayList<>();
+        populatePostionCordinates();
+    }
+
+    private void populatePostionCordinates(){
+
+        PositionCordinates.add(new Pair<>(0,0));
+        for (int i = 0; i < SnakeLadder.height; i++) {
+            for (int j = 0; j < SnakeLadder.width; j++) {
+                int xCord =0;
+                if(i%2==0) {
+                    xCord = j * SnakeLadder.tilesize + SnakeLadder.tilesize / 2;
+                }
+                else {
+
+                    xCord = SnakeLadder.tilesize * SnakeLadder.height - (j*SnakeLadder.tilesize) - SnakeLadder.tilesize/2;
+
+                }
+                int yCord = SnakeLadder.tilesize * SnakeLadder.height - (i*SnakeLadder.tilesize) - SnakeLadder.tilesize/2;
+                PositionCordinates.add(new Pair<>(xCord,yCord));
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        for (int i = 0; i < board.PositionCordinates.size(); i++) {
+            System.out.println(i + " $  x : " +  board.PositionCordinates.get(i).getKey() + "  Y : " + board.PositionCordinates.get(i).getValue());
+
+        }
+    }
+}
